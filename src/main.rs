3b768046d77272f751085262f3599ec92668925f
404b0tnet <jsmith728.js@gmail.com>
2021-11-03 17:19:13 -0400

    created calculator app

--- src/main.rs
+++ src/main.rs
@@ -8,17 +8,54 @@
 // i16  (-32,768 => 32,767)
 // i32  (-2,147,483,648 => 2,147,483,647)
 // i64  (-9,223,372,036,854,775,808 => 9,223,372,036,854,775,807)
-// i128 (-170,141,183,460,469,231,731,687,303,715,884,105,728 
+// i128 (-170,141,183,460,469,231,731,687,303,715,884,105,728
 //          => 170,141,183,460,469,231,731,687,303,715,884,105,727)
 
+use std::io::{stdin, stdout, Write};
 
 fn main() {
-    let mut x = 5;
-    println!("The value of x is: {}", x);
-    x = 6;
-    println!("The value of x is: {}", x);
+    // title
+    println!("welcome to the calculator");
+    println!("----------");
 
-    x = x + 4;
-    println!("The new value of x is {}.", x);
+    // calculation variables
+    let mut num1 = String::new();
+    let mut num2 = String::new();
+    let mut operator = String::new();
+
+    loop {
+        // get user input
+        println!("what is the first number? ");
+        read(&mut num1);
+        println!("what is the second number? ");
+        read(&mut num2);
+        println!("what operationg would you like to do? [*+-/]:  ");
+        read(&mut operator);
+
+        // clean up input
+        let num1: f32 = num1.trim().parse().unwrap();
+        let num2: f32 = num2.trim().parse().unwrap();
+        let operator: char = operator.trim().chars().next().unwrap();
+
+        let operators = String::from("+-*/");
+        if !operators.contains(operator) {
+            println!("unknown operator");
+            return;
+        }
+
+        let result = match operator {
+            '+' => num1 + num2,
+            '-' => num1 - num2,
+            '*' => num1 * num2,
+            '/' => num1 / num2,
+            _ => panic!("error in operator"),
+        };
+
+        println!("the results of {} {} {} = {}", num1, operator, num2, result);
+    }
 }
 
+fn read(input: &mut String) {
+    stdout().flush().expect("failed to flush");
+    stdin().read_line(input).expect("failed to read");
+}
